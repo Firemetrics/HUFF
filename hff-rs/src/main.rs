4b1561c4fa9ee3c7687eddbd62fc78785065dbb9
Nicolas Kamzol <40169577+nickamzol@users.noreply.github.com>
2025-03-03 14:39:48 +0100

    fix(cli): borrow in match to fix compiler error

--- hff-rs/src/main.rs
+++ hff-rs/src/main.rs
@@ -29,7 +29,7 @@ fn main() {
         }
     }
 
-    match serde_json::from_str(&buffer) {
+    match &serde_json::from_str(&buffer) {
         Ok(response) => match args.mapping {
             Some(mapping) => {
                 println!(
