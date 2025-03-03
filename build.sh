f0a9ba09e4c44f673ae905c2d11e07f98effbe4c
Nicolas Kamzol <40169577+nickamzol@users.noreply.github.com>
2025-03-03 14:42:23 +0100

    fix(build): use cli feature

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 cd hff-rs
 wasm-pack build --release --target web --out-name hff-wasm --out-dir ../chrome-extension/src/static -- --features wasm
-cargo build --release
+cargo build --release --features cli
